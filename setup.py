b8e7160511ac47742ef124b4f0edafd1a6dfe8c4
Joseph Margaryan <josephmargaryan@MacBook-Air-tilhrende-Joseph.local>
2024-12-26 14:44:41 +0100

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ChronosForge",
-    version="0.1.0",
-    packages=find_packages(),
+    name="ChronosForge",  # Package name
+    version="0.1.0",      # Version
+    description="A library for probabilistic and Bayesian machine learning models",
+    author="Joseph Margaryan",
+    author_email="your-email@example.com",
+    url="https://github.com/josephmargaryan/ChronosForge",  # GitHub URL
+    packages=find_packages(),  # Automatically find all subpackages
     install_requires=[
         "numpy",
         "jax",
         "numpyro",
         "torch",
         "matplotlib",
-        "pandas",
         "pyro-ppl",
-        "scikit-learn"
-    ],
+        "pandas"
+    ],  
+    python_requires=">=3.7",  # Python version compatibility
 )
 
