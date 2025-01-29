0666002e39a85510904344eac17bf6850148f6cd
Joseph Margaryan <josephmargaryan@MacBook-Air-tilhrende-Joseph.local>
2025-01-29 17:31:28 +0100

    Exposing modules to the top directory

--- quantbayes/__init__.py
+++ quantbayes/__init__.py
@@ -1,4 +1,30 @@
-from .bnn import *  # Import everything from `bnn`
+from . import bnn
 from . import forecast
+from . import dmm
+from . import generative
+from . import gmm
+from . import hilbert_space
+from . import long_seq_classifier
+from . import rl
+from . import sde
+from . import similarity_tools
+from . import stochax
+from . import vae
+from . import var
 
-__all__ = ["bnn", "forecast"]  # Only expose `bnn` at the root level
+__all__ = [
+    "bnn",
+    "forecast",
+    "dmm",
+    "generative",
+    "gmm",
+    "hilbert_space",
+    "long_seq_classifier",
+    "rl",
+    "sde",
+    "similarity_tools",
+    "stochax",
+    "vae",
+    "var",
+]
+
