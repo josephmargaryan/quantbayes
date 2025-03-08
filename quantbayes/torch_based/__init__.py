83acb1652c437c666afbf3bad876b59edbec0a3f
Joseph Margaryan <jmar@baselifescience.com>
2025-03-08 14:55:46 +0100

    Add torch trainer

--- quantbayes/torch_based/__init__.py
+++ quantbayes/torch_based/__init__.py
@@ -2,10 +2,11 @@ from . import image_segmentation
 from . import image_classification
 from . import tabular
 from . import diffusion
-
+from .trainer.trainer import Trainer
 __all__ = [
     "image_segmentation",
     "image_classification",
     "tabular",
     "diffusion",
+    "Trainer"
 ]
