13100203ea7660970e54712f7bc7d8b2bf3247b9
Joseph Margaryan <joe.2010@live.dk>
2025-07-26 16:39:03 +0200

    Replaced absolute imports with relative import paths

--- quantbayes/stochax/diffusion/__init__.py
+++ quantbayes/stochax/diffusion/__init__.py
@@ -1,26 +1,26 @@
-from quantbayes.stochax.diffusion import generate
-from quantbayes.stochax.diffusion.config import (
+from . import generate
+from .config import (
     ImageConfig,
     TabularConfig,
     TimeSeriesConfig,
 )
-from quantbayes.stochax.diffusion.dataloaders import (
+from .dataloaders import (
     dataloader,
     generate_synthetic_image_dataset,
 )
-from quantbayes.stochax.diffusion.models.adaptive_DiT import DiT
-from quantbayes.stochax.diffusion.models.mixer_2d import Mixer2d
-from quantbayes.stochax.diffusion.models.mlp import DiffusionMLP
-from quantbayes.stochax.diffusion.models.times_series_1d import ConvTimeUNet
-from quantbayes.stochax.diffusion.models.transformer_1d import DiffusionTransformer1D
-from quantbayes.stochax.diffusion.models.transformer_2d import DiffusionTransformer2D
-from quantbayes.stochax.diffusion.models.unet_2d import UNet
-from quantbayes.stochax.diffusion.sde import (
+from .models.adaptive_DiT import DiT
+from .models.mixer_2d import Mixer2d
+from .models.mlp import DiffusionMLP
+from .models.times_series_1d import ConvTimeUNet
+from .models.transformer_1d import DiffusionTransformer1D
+from .models.transformer_2d import DiffusionTransformer2D
+from .models.unet_2d import UNet
+from .sde import (
     int_beta_linear,
     single_sample_fn,
     weight_fn,
 )
-from quantbayes.stochax.diffusion.trainer import train_model
+from .trainer import train_model
 
 __all__ = [
     "generate",
